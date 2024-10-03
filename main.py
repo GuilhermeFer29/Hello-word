c217f240820e1b4f6a426d07032eb567335f9ed4
GuilhermeFer29 <leciguilherme@gmail.com>
2024-10-03 20:24:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def chamada_ola():
   print("Hello, World!")
 
 
+<<<<<<< Updated upstream
 def adeus():
   print('Goodbye, World')
 
 chamada_ola()
 adeus()
+=======
+
+
+chamada_ola()
+>>>>>>> Stashed changes
